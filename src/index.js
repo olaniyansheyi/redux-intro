b6452190e0c0e44363911862f78a3edcdf84d900
sheymantech <olaniyansheyi1704@gmail.com>
2024-03-30 19:35:26 +0100

    new feature

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+import { Provider } from "react-redux";
 import store from "./Store";
 
 store.dispatch({
@@ -15,6 +15,8 @@ console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Provider store={store} >
     <App />
+    </Provider>
   </React.StrictMode>
 );
